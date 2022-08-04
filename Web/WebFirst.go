abdda25f499b28d50031da6c20f26be4cdf3bd39
y <y>
2022-08-04 22:10:11 +0900

    コミット

---
+++ Web/WebFirst.go
@@ -0,0 +1,19 @@
+package main
+
+
+import (
+	"fmt"
+	//パッケージ
+)
+
+func main(){
+	//変数,変数 := http.Get(アドレス)
+	//上記で取得した場合、以下４つに分けられる
+	//1,Status→アクセスの状態
+	//2,StatusCode→ステータスコードを表すint値
+	//3,Body→コンテンツ部分
+	//4,ContentLength→コンテンツの長さを表す
+	
+	fmt.Println("Hello Go-lang!")
+
+}
